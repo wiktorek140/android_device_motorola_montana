a7470488c69c7cd143af26d34d8c8c21652b22a5
Wiktor Strzębała <wiktorek140@tlen.pl>
2021-01-18 07:38:11 +0100

    montana: enable ril debug messages
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>
    Change-Id: I2a21e15fb983a0218bdb0b44a306d68c95c0d605

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -14,6 +14,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.disable_rescue=true
 
+# RIL debug
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.ims.disableADBLogs=0 \
+    persist.vendor.ims.disableDebugDataPathLogs=0 \
+    persist.vendor.ims.disableDebugLogs=0 \
+    persist.vendor.ims.disableIMSLogs=0 \
+    persist.vendor.ims.disableQXDMLogs=1 \
+    persist.vendor.ims.vt.enableadb=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.sensors=montana
